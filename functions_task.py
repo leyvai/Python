934a18094b238396e64026c5dd366160c814ec58
leyvai <bu50.88@gmail.com>
2019-06-28 16:05:41 +0300

    Added Functions Task

---
+++ functions_task.py
@@ -0,0 +1,24 @@
+import datetime
+
+year = raw_input ("Enter year of birth: ")
+month = raw_input ("Enter month of birth: ")
+day = raw_input ("Enter day of birth: ")
+bday = datetime.date(int(year),int(month),int(day))
+today = datetime.datetime.today()
+todate = today.date()
+
+def check_birthday():
+	if todate < bday:
+		print "Youz a fake"
+	return False
+
+def calculate_age():
+	age_year = todate.year - bday.year
+	age_month = todate.month - bday.month
+	age_day = todate.day - bday.day
+	
+
+	print ("You are " + str(age_year) + " years, " + str(age_month) + " months, and " + str(age_day) + " days old")
+
+check_birthday()
+calculate_age()
